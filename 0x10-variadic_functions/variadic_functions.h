194f3e7684958ef44aa0947653e4dd4436f05409
Francis <fhrancorey99@gmail.com>
2022-09-24 11:25:52 +0100

    new file commits on variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void print_int(va_list list);
+void print_char(va_list list);
+void print_float(va_list list);
+void print_string(va_list list);
+
+/**
+ * 
+ * 
+ */
+
+typedef struct PrintTypeStruct
+{
+    char *type;
+    void(*printer)(va_list);
+} PrintTypeStruct;
+
+#endif
