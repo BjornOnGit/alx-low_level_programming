c0062cf073faed434caf0b43cdfabe577098e29d
Francis <fhrancorey99@gmail.com>
2023-01-12 08:37:38 +0100

    upgrade high ho

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,24 +16,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int op, wr, len = 0;
+	int op, wr, len = 0;
 
-    if (filename == NULL);
-            return (-1);
-    
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len];)
-                len++;
-    }
+	if (filename == NULL)
+		return (-1);
 
-    op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    wr = write(op, text_content, len);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    if (op == -1 || wr == -1)
-            return (-1);
-    
-    close(op);
-    
-    return (1);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
 }
