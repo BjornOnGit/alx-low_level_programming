f3f7b036e4069eba07553e83336f5e8b3e358dd0
Francis <fhrancorey99@gmail.com>
2023-05-05 05:44:07 +0100

    feat: funcn sets bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+/**
+* File: 3-set_bit.c
+* Auth: Eze Francis Ogonnaya
+*/
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * set_bit - it sets the value of a bit at a given index to 1
+  * @n: the value of the bit.
+  * @index: the index of the bit
+  * Return: -1 if there is an error, otherwise, bit is set to 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 0 || index >= sizeof(unsigned int) * 8)
+	{
+		return (-1);
+	}
+	*n ^= (1 << index);
+	return (1);
+}
