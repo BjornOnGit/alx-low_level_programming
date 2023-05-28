2cdc7b89400968ad72f94fcccf7749ceb0a88b07
Francis <fhrancorey99@gmail.com>
2023-05-28 03:12:21 +0100

    deferment

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 
 /**
  * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
+ * @argc - The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
